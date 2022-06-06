f9a546d1259dd7aac29b196003dde7853e4faae7
mishkamag <m93.maglaperidze@gmail.com>
2022-06-06 20:55:54 +0400

    Calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+let outputScreen = document.getElementById("output-screen");
+
+function display(num) {
+  outputScreen.value += num;
+} /*ციფრების დაწერა და მიწეპება ერთმანეთზე */
+
+function Calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    alert("Invalid");
+  }
+}
+
+function Clear() {
+  outputScreen.value = "";
+}
+
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
